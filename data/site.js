0a751f5712cd0ca12a0df6c4ba55577cfd362528
Александр Мельк <alexmelk@yandex.ru>
2020-03-21 11:43:11 +0300

    update data

--- data/site.js
+++ data/site.js
@@ -1,4 +1,17 @@
-﻿
+﻿var data = [];
+var labels = [];
+var ctx;
+var chart;
+var maxY = 26;
+var minY = 0;
+var minTemp;
+var maxTemp;
+
+var getUARTdataEv;
+var getUARTrawEv;
+var stopFlag = false;
+var n = 1966;
+
 window.onload = async function()
 {
 	//append setting container
@@ -14,11 +27,9 @@ window.onload = async function()
 	//$("#inputGroupSelect01").empty();
 	//$("#inputGroupSelect01").append(t);
 	//var wifilist = setInterval(await getWiFiList,60000);
-	
-	setInterval(await getUARTdata,1000);
 	//setInterval(await getDevicesList,60000);
 	//setInterval(await getNotifi,40000);
-
+	getUARTdataEv = setInterval(await getUARTdata,1000);
 
 	$("#sendToUART").on("click",function(){
 		let data = $("#usersMess").val();
@@ -32,14 +43,73 @@ window.onload = async function()
 		fetch("/configUART?UARTspeed="+data);
 	})
 
-
+	this.updateChart();
 
 }
 
 async function getUARTdata()
 {
-	response = await fetch("/getUARTdata");
-	t = await response.text();
+	let response = await fetch("/getUARTdata");
+	let t = await response.text();
 	console.log(t);
 	if(t){$("#containerNotifi").append(t)};
+
+	let mas = t.match(/\d\d+/g);
+	removeData();
+	mas.shift();
+	for(let i = 0; i < mas.length; i++)
+	{
+		data.push((mas[i]-n)/18.68);
+		labels.push(i);
+	}
+	
+	console.log(mas);
+	chart.update();
+}
+
+function updateChart()
+{
+
+	ctx = document.getElementById('myChart').getContext('2d');
+	chart = new Chart(ctx, {
+		// The type of chart we want to create
+		type: 'line',
+		
+		// The data for our dataset
+		data: {
+			labels: labels,
+			datasets: [{
+				label: 'График',
+				backgroundColor: 'rgb(255, 99, 132)',
+				borderColor: 'rgb(255, 99, 132)',
+				data: data,
+				fill: false,
+			}],
+		}
+	});
+}
+async function toogle(element)
+{
+	console.log(element)
+	if($("#"+element.id).hasClass("btn-danger"))//остановлено
+	{
+		$("#"+element.id)[0].innerText = "Останов.";
+		$("#"+element.id).removeClass("btn-danger");
+		$("#"+element.id).addClass("btn-primary");
+		getUARTdataEv = setInterval(getUARTdata,1000);
+	}
+	else
+	{
+		$("#"+element.id)[0].innerText = "Пуск";
+		$("#"+element.id).removeClass("btn-primary");
+		$("#"+element.id).addClass("btn-danger");
+		clearInterval(getUARTdataEv);
+	}
+}
+function removeData() {
+	while(data.length){
+		data.pop();
+		labels.pop();
+	}
+    chart.update();
 }
